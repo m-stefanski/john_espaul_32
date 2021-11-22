57087cf73d37471c29e2eeb59ade2d4d61fc2cd2
Marcin Stefański <m_stefanski@icloud.com>
2021-11-22 09:18:20 +0100

    fest: all timers now asynchronous

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
-Button2 btn1;
-Button2 btn2;
+Button2 btnLeft;
+Button2 btnRight;
 
-hw_timer_t * timer = NULL;
-volatile byte state = LOW;
+hw_timer_t * timerEraseText = NULL;
+hw_timer_t * timerEraseEyes = NULL;
 
 char *papieskieMysli[] = {
     "Tso?",
@@ -23,14 +23,56 @@ char *papieskieMysli[] = {
     "Okrutnik"
 };
 
-void IRAM_ATTR onTimer(){
-  state = !state;
-  if(state){
-    tft.drawCentreString("...", 70, 0, 2);
-  }
-  else {
-    tft.drawCentreString("   ", 70, 0, 2);
-  }
+void drawPapa(){
+    tft.fillScreen(TFT_BLACK);
+    tft.fillEllipse(70, 280, 70, 140, TFT_WHITE);
+    tft.fillEllipse(70, 140, 30, 40, TFT_ORANGE);
+    tft.drawCentreString("JP2 8-ball", 70, 0, 2);
+}
+
+void fillTextArea(){
+    tft.drawCentreString(papieskieMysli[random(7)], 70, 60, 2);
+    Serial.println("Filled text area");
+}
+
+void eraseTextArea(){
+    tft.fillRect(0, 60, 160, 20, TFT_BLACK);
+    Serial.println("Erased text area");
+}
+
+void IRAM_ATTR onTimerEraseText(){
+    eraseTextArea();
+}
+
+void createTextTimer(){
+    timerEraseText = timerBegin(0, 80, true);
+    timerAttachInterrupt(timerEraseText, &onTimerEraseText, true);
+    timerAlarmWrite(timerEraseText, 2000000, false);
+    timerAlarmEnable(timerEraseText);
+    Serial.println("Text timer created");
+}
+
+void fillEyesArea(){
+    tft.fillEllipse(60, 140, 3, 3, TFT_RED);
+    tft.fillEllipse(80, 140, 3, 3, TFT_RED);
+    Serial.println("Filled eyes area");
+}
+
+void eraseEyesArea(){
+    tft.fillEllipse(70, 140, 30, 40, TFT_ORANGE);
+    Serial.println("Erased eyes area");
+}
+
+void IRAM_ATTR onTimerEraseEyes(){
+    eraseEyesArea();
+}
+
+void createEyeTimer(){
+    timerEraseEyes = timerBegin(1, 80, true);
+    timerAttachInterrupt(timerEraseEyes, &onTimerEraseEyes, true);
+    timerAlarmWrite(timerEraseEyes, 1000000, false);
+    timerAlarmEnable(timerEraseEyes);
+    Serial.println("Eye timer created");
 }
 
 void setup()
@@ -39,39 +81,29 @@ void setup()
     Serial.println("Initialized");
     tft.begin();
     tft.setRotation(0);	
-    tft.fillScreen(TFT_BLACK);
-    tft.setSwapBytes(true);
 
-    tft.fillEllipse(70, 280, 70, 140, TFT_WHITE);
-    tft.fillEllipse(70, 140, 30, 40, TFT_ORANGE);
+    btnLeft.begin(BUTTON_1);
+    btnRight.begin(BUTTON_2);
 
-    btn1.begin(BUTTON_1);
-    btn2.begin(BUTTON_2);
-
-    btn1.setTapHandler([](Button2 & b) {
-        tft.drawCentreString(papieskieMysli[random(7)], 70, 60, 2);
-        delay(2000);
-        tft.fillRect(0, 60, 160, 20, TFT_BLACK);
+    btnLeft.setTapHandler([](Button2 & b) {
+        eraseTextArea();
+        fillTextArea();
+        createTextTimer();
     });
 
-    btn2.setTapHandler([](Button2 & b) {
-        tft.fillEllipse(60, 140, 3, 3, TFT_RED);
-        tft.fillEllipse(80, 140, 3, 3, TFT_RED);
-        delay(1000);
-        tft.fillEllipse(70, 140, 30, 40, TFT_ORANGE);
+    btnRight.setTapHandler([](Button2 & b) {
+        fillEyesArea();
+        createEyeTimer();
     });
-
-    timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(timer, &onTimer, true);
-    timerAlarmWrite(timer, 1000000, true);
-    timerAlarmEnable(timer);
 
     randomSeed(analogRead(1));
+
+    drawPapa();
 }
 
 void loop()
 {
-    btn1.loop();
-    btn2.loop();
+    btnLeft.loop();
+    btnRight.loop();
 }
 
